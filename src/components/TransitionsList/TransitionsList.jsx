d965861b6af552098057ac5e862bdfae1eac4272
Dasha Yarkovska <yarkovska11@gmail.com>
2023-03-06 22:05:46 +0200

    fix table tags

--- src/components/TransitionsList/TransitionsList.jsx
+++ src/components/TransitionsList/TransitionsList.jsx
@@ -16,43 +16,49 @@ export const TransactionsList = () => {
   };
   return (
     <table>
-      <tr>
-        <th>Date</th>
-        <th>Type</th>
-        <th>Category</th>
-        <th>Comment</th>
-        <th>Sum</th>
-      </tr>
-      {transitions.map(
-        ({ id, transactionDate, type, categoryId, comment, amount }) => (
-          <tr key={id}>
-            <td>{transactionDate}</td>
-            <td>{type}</td>
-            <td>{categoryId}</td>
-            <td>{comment}</td>
-            <td>{amount}</td>
-            <td>
-              <button
-                onClick={() =>
-                  handleEditTransition({
-                    id,
-                    transactionDate,
-                    type,
-                    categoryId,
-                    comment,
-                    amount,
-                  })
-                }
-              >
-                Edit
-              </button>
-            </td>
-            <td>
-              <button onClick={() => handleDeleteTransition(id)}>Delete</button>
-            </td>
-          </tr>
-        )
-      )}
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Type</th>
+          <th>Category</th>
+          <th>Comment</th>
+          <th>Sum</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transitions.map(
+          ({ id, transactionDate, type, categoryId, comment, amount }) => (
+            <tr key={id}>
+              <td>{transactionDate}</td>
+              <td>{type}</td>
+              <td>{categoryId}</td>
+              <td>{comment}</td>
+              <td>{amount}</td>
+              <td>
+                <button
+                  onClick={() =>
+                    handleEditTransition({
+                      id,
+                      transactionDate,
+                      type,
+                      categoryId,
+                      comment,
+                      amount,
+                    })
+                  }
+                >
+                  Edit
+                </button>
+              </td>
+              <td>
+                <button onClick={() => handleDeleteTransition(id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          )
+        )}
+      </tbody>
     </table>
   );
 };
