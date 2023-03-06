2dde86e9e866a0718fb0a67cc326d8a54594ad05
Iryna <aripluss@i.ua>
2023-03-06 15:37:31 +0200

    Change api base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_DOMAIN;
-
+const API_BASE_URL = 'https://wallet.goit.ua';
+// const API_BASE_URL = process.env.REACT_APP_API_DOMAIN;
 const $publicHost = axios.create({
   baseURL: API_BASE_URL,
   headers: {
