e07b8b014a326332edb903ddd21e4a652d6d3f96
seannse <seannse@gmail.com>
2023-03-08 01:00:53 +0200

    modalTeam

--- src/components/ModalTeam/ModalTeam.jsx
+++ src/components/ModalTeam/ModalTeam.jsx
@@ -1,47 +1,88 @@
 import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ModalCloseBtn } from 'reusable';
-import { Modal } from './ModalTeam.styled';
 import { creators } from './teamMembers';
-import { isModalAddTransactionOpen } from 'redux/global/globalSlice';
+import { toggleModalTeam } from 'redux/global/globalSlice';
+import { BackDrop, Modal } from './ModalTeam.styled';
 
 function ModalTeam() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (currentIndex > creators.length) {
+      setCurrentIndex(0);
+    }
+
+    let slider = setInterval(
+      () => setCurrentIndex(prevState => prevState + 1),
+      3500
+    );
+    return () => {
+      clearInterval(slider);
+    };
+  }, [currentIndex]);
+
   useEffect(() => {
     const onEscClose = e => {
       if (e.code === 'Escape') {
-        dispatch(isModalAddTransactionOpen());
+        dispatch(toggleModalTeam());
       }
     };
+
     document.addEventListener('keydown', onEscClose);
     return () => document.removeEventListener('keydown', onEscClose);
   }, [dispatch]);
 
+  function sliderMove(id) {
+    let position = 'nextSlide';
+    if (id === currentIndex) {
+      position = 'activeSlide';
+    }
+
+    if (
+      id === currentIndex - 1 ||
+      (currentIndex === 0 && id === creators.length)
+    ) {
+      // console.log(currentIndex);
+      position =
+        currentIndex % 2 === 0 || !currentIndex
+          ? 'lastSlideLeft'
+          : 'lastSlideRight';
+    }
+    return position;
+  }
+
   return (
-    <Modal>
-      <ModalCloseBtn />
-      <h2>This website creators</h2>
-      <div>
-        <ul>
-          {creators.map(({ name, role, src, id }) => {
-            return (
-              <li key={id}>
-                <img
-                  src={require('images/team/Dasha_Yarkovska.jpg')}
-                  alt={name}
-                  width="85"
-                  loading="lazy"
-                />
-                <div>
-                  <p>{name}</p>
-                  <p>{role}</p>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </Modal>
+    <BackDrop
+      onClick={e => {
+        if (e.target === e.currentTarget) {
+          dispatch(toggleModalTeam());
+        }
+      }}
+    >
+      <Modal>
+        <ModalCloseBtn type="button" isRandomModalOpen={toggleModalTeam} />
+        <h2>This website creators</h2>
+        <div>
+          <ul>
+            {creators.map(({ name, role, src, id }) => {
+              const position = sliderMove(id);
+              return (
+                <li key={id} className={position}>
+                  <img src={src} alt={name} width="85" loading="lazy" />
+                  <div>
+                    <p>{name}</p>
+                    <p>{role}</p>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </Modal>
+    </BackDrop>
   );
 }
 
