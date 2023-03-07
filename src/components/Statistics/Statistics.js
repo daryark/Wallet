182a29b86b7ee1b930cb6b55523f22e584723c32
ValeriaSarazhinska <lera.sarazhinska@gmail.com>
2023-03-07 13:43:48 +0200

    Refactor Select input

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { ArcElement, Chart as ChartJS, Legend, Tooltip } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
 import expenses from './expenses.json';
 import Select from 'react-select';
-import styled from 'styled-components';
+import {Container, Title, customStyles} from './Statistics.styled.js'
+import {useDispatch, useSelector} from "react-redux";
+import {selectCurrency} from "../../redux/monobank/mono-selectors";
+import {selectSummary} from "../../redux/transactions/trans-selectors";
+import {getTransactionSummary} from "../../redux/transactions/trans-operations";
+
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -14,126 +19,98 @@ export const data = {
       data: expenses.map(expense => expense.percent),
       backgroundColor: expenses.map(expense => expense.color),
       borderColor: expenses.map(expense => expense.color),
-      borderWidth: 1,
+      borderWidth:1,
     },
   ],
 };
 
-const plugins = [
-  {
-    beforeDraw: function ({ width, height, ctx }) {
-      ctx.restore();
-      ctx.font = 20 + 'px sans-serif';
-      ctx.textBaseline = 'top';
-      const text = '₴ 24 000.00',
-        textX = Math.round((width - ctx.measureText(text).width) / 2),
-        textY = height / 2;
-      ctx.fillText(text, textX, textY);
-      ctx.save();
-    },
-  },
+
+const plugins = [{
+  beforeDraw: function({width, height,ctx}) {
+    ctx.restore();
+    ctx.font = 20 + "px sans-serif";
+    ctx.textBaseline = "top";
+    const text = "₴ 24 000.00",
+      textX = Math.round((width - ctx.measureText(text).width) / 2),
+      textY = height / 2;
+    ctx.fillText(text, textX, textY);
+    ctx.save();
+  }
+}]
+
+const monthData = [
+  { value: 0, label: 'January' },
+  { value: 1, label: 'February' },
+  { value: 2, label: 'March' },
+  { value: 3, label: 'April' },
+  { value: 4, label: 'May' },
+  { value: 5, label: 'June' },
+  { value: 6, label: 'July' },
+  { value: 7, label: 'August' },
+  { value: 8, label: 'September' },
+  { value: 9, label: 'October' },
+  { value: 10, label: 'November' },
+  { value: 11, label: 'December' },
 ];
 
-const month = [
-  { value: 'January', label: 'January' },
-  { value: 'February', label: 'February' },
-  { value: 'March', label: 'March' },
-  { value: 'April', label: 'April' },
-  { value: 'May', label: 'May' },
-  { value: 'June', label: 'June' },
-  { value: 'July', label: 'July' },
-  { value: 'August', label: 'August' },
-  { value: 'September', label: 'September' },
-  { value: 'October', label: 'October' },
-  { value: 'November', label: 'November' },
-  { value: 'December', label: 'December' },
+const yearData = [
+  { value: 2022, label: '2022' },
+  { value: 2023, label: '2023' },
 ];
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  width: 380px;
-  height: 380px;
-`;
-const Title = styled.h1`
-  font-weight: 400;
-  font-size: 30px;
-  margin-bottom: 8px;
-`;
 
-const customStyles = {
-  control: provided => ({
-    ...provided,
-    width: '230px',
-    height: '40px',
-    borderRadius: '30px',
-    border: 'solid 1px #000000',
-    cursor: 'pointer',
-    '&:hover': {
-      borderColor: '#D8093A',
-    },
-  }),
-  option: (provided, state) => ({
-    ...provided,
-    backgroundColor: state.isSelected ? '#dedede' : 'transparent',
-    color: state.isSelected ? '#333333' : '#666666',
-
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    padding: '2px 103px 2px 20px',
-    gap: '10px', //
-    '&:hover': {
-      backgroundColor: '#dedede',
-      color: '#333333',
-    },
-  }),
-  singleValue: provided => ({
-    ...provided,
-    color: '#333333',
-  }),
-  dropdownIndicator: provided => ({
-    ...provided,
-    color: '#333333',
-    '&:hover': {
-      color: '#D8093A',
-    },
-  }),
-  indicatorSeparator: () => ({
-    display: 'none',
-  }),
-  menu: provided => ({
-    ...provided,
-    width: '230px',
-    borderRadius: '30px',
-    overflow: 'hidden',
-  }),
-};
 
 export const Statistics = () => {
-  const [selectedOption, setSelectedOption] = useState(null);
+  console.log((new Date).getMonth())
+  console.log((new Date).getFullYear())
+  // const { response, months } = useSelector(selectSummary);
+  // console.log(response, months )
+  //
+  // const options = months.map(month => ({
+  //   value: month,
+  //   label: month
+  // }));
 
-  const handleChange = selectedOption => {
-    setSelectedOption(selectedOption);
+  const [month, setMonth] = useState((new Date).getMonth());
+  const [year, setYear] = useState((new Date).getFullYear());
+
+  useEffect(()=>{
+
+  }, [])
+
+  const handleMonthChange = ({value}) => {
+    setMonth(value);
+  };
+  const handleYearChange = ({value}) => {
+    setYear(value);
   };
 
   return (
     <>
       <Container>
         <Title>Statistics</Title>
-        <Doughnut data={data} plugins={plugins} type={'doughnut'} />
+        <Doughnut data={data}  plugins={plugins} type={"doughnut"}/>
       </Container>
       <Container>
         <Select
-          value={selectedOption}
-          onChange={handleChange}
-          options={month}
+          // defaultInputValue={monthData[month].label}
+          value={month}
+          onChange={handleMonthChange}
+          options={monthData}
           placeholder="Select a month"
+          styles={customStyles}
+        />
+        <Select
+          // defaultInputValue={yearData[year].label}
+          value={year}
+          onChange={handleYearChange}
+          options={yearData}
+          placeholder="Select a year"
           styles={customStyles}
         />
       </Container>
     </>
-  );
-};
+
+
+  )
+}
