9fd4bb66461d96abf4bb19398881d60afbf8f459
Dasha Yarkovska <yarkovska11@gmail.com>
2023-03-08 22:00:23 +0200

    fix unused

--- src/redux/transactions/trans-operations.js
+++ src/redux/transactions/trans-operations.js
@@ -5,7 +5,7 @@ import {
   TransactionCategoriesAPI,
   TransactionSummaryAPI,
 } from 'services/api';
-import {log10} from "chart.js/helpers";
+// import {log10} from "chart.js/helpers";
 
 export const fetchTransactions = createAsyncThunk(
   'transactions/getTransactions',
@@ -75,9 +75,10 @@ export const getTransactionSummary = createAsyncThunk(
   async (formData, { rejectWithValue }) => {
     try {
       // return await TransactionSummaryAPI.getTransactionSummary(formData);
-      const response  = await TransactionSummaryAPI.getTransactionSummary(formData
+      const response = await TransactionSummaryAPI.getTransactionSummary(
+        formData
       );
-      console.log(response)
+      console.log(response);
       return response;
     } catch (error) {
       return rejectWithValue(error.message);
