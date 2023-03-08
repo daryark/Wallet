bb71f71d992bf5a2268a805b0caa8e0d47a58a1a
Dasha Yarkovska <yarkovska11@gmail.com>
2023-03-08 19:05:18 +0200

    fix

--- src/redux/transactions/trans-operations.js
+++ src/redux/transactions/trans-operations.js
@@ -73,7 +73,7 @@ export const getTransactionSummary = createAsyncThunk(
   'transactions/getTransactionSummary',
   async (formData, { rejectWithValue }) => {
     try {
-      return await TransactionSummaryAPI.getTransactionSummary(formData);
+      // return await TransactionSummaryAPI.getTransactionSummary(formData);
       const { response } = await TransactionSummaryAPI.getTransactionSummary(
         formData
       );
