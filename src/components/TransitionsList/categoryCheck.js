c6ff54038aa833a29ff421e9df430fc15f5c233b
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-11 01:42:35 +0200

    Create categoryCheck.js

---
+++ src/components/TransitionsList/categoryCheck.js
@@ -0,0 +1,45 @@
+export const categoryCheck = function (category) {
+  let categ = '';
+  switch (category) {
+    case 'Education':
+      categ = 'Освіта';
+      break;
+    case 'Main expenses':
+      categ = 'Основні витрати';
+      break;
+    case 'Products':
+      categ = 'Продукти';
+      break;
+    case 'Car':
+      categ = 'Машина';
+      break;
+    case 'Self care':
+      categ = 'Турбота про себе';
+      break;
+    case 'Child care':
+      categ = 'Турбота про дитину';
+      break;
+    case 'Household products':
+      categ = 'Побутові вироби';
+      break;
+    case 'Leisure':
+      categ = 'Розваги';
+      break;
+    default:
+      categ = 'Інші витрати';
+  }
+  return categ;
+};
+
+export const typeCheck = function (type) {
+  let catType = '';
+  switch (type) {
+    case 'INCOME':
+      catType = 'Дохід';
+      break;
+
+    default:
+      catType = 'Витрати';
+  }
+  return catType;
+};
