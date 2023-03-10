4a5308c79b88c25da884d437b00ae5bc7aeeaeb9
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-11 01:42:26 +0200

    Update ModalTeam.jsx

--- src/components/Layout/Footer/ModalTeam/ModalTeam.jsx
+++ src/components/Layout/Footer/ModalTeam/ModalTeam.jsx
@@ -1,8 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { creators } from './teamMembers';
+import { creators, creatorsUkr } from './teamMembers';
 import { toggleModalTeam } from 'redux/global/globalSlice';
-import { selectIsModalTeamOpen } from 'redux/global/global-selectors';
+import {
+  selectIsModalTeamOpen,
+  selectLanguage,
+} from 'redux/global/global-selectors';
 import { ModalCloseBtn } from 'reusable';
 import ModalBackdrop from 'components/ModalBackdrop/ModalBackdrop';
 import { ModalStyled } from './ModalTeam.styled';
@@ -12,6 +15,7 @@ import { useTranslation } from 'react-i18next';
 export function ModalTeam() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const isModalOpen = useSelector(selectIsModalTeamOpen);
+  const lan = useSelector(selectLanguage);
 
   const dispatch = useDispatch();
   const { t } = useTranslation();
@@ -69,18 +73,33 @@ export function ModalTeam() {
             <h2>{t('modalTeamTitle')}</h2>
             <div>
               <ul>
-                {creators.map(({ name, role, src, id }) => {
-                  const position = sliderMove(id);
-                  return (
-                    <li key={id} className={position}>
-                      <img src={src} alt={name} width="85" loading="lazy" />
-                      <div>
-                        <p>{name}</p>
-                        <p>{role}</p>
-                      </div>
-                    </li>
-                  );
-                })}
+                {lan === true &&
+                  creators.map(({ name, role, src, id }) => {
+                    const position = sliderMove(id);
+                    return (
+                      <li key={id} className={position}>
+                        <img src={src} alt={name} width="85" loading="lazy" />
+                        <div>
+                          <p>{name}</p>
+                          <p>{role}</p>
+                        </div>
+                      </li>
+                    );
+                  })}
+
+                {lan === false &&
+                  creatorsUkr.map(({ name, role, src, id }) => {
+                    const position = sliderMove(id);
+                    return (
+                      <li key={id} className={position}>
+                        <img src={src} alt={name} width="85" loading="lazy" />
+                        <div>
+                          <p>{name}</p>
+                          <p>{role}</p>
+                        </div>
+                      </li>
+                    );
+                  })}
               </ul>
             </div>
           </ModalStyled>
