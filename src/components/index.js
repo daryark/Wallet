5ee2974e803bc8bdb0af9548035ee7eacd28d38c
Firsova Liubov <firsova.lyub@gmail.com>
2023-03-10 13:03:48 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,16 @@
 export * from 'components/Layout/Header/Header';
+export * from 'components/Layout/SideBar/SideBar.jsx';
 export * from 'components/Loader/Loader';
-export * from 'components/TransitionsList/TransitionsList';
-export * from 'components/TransitionsList/TransitionsListMobile';
+
 export * from 'components/LoginForm/LoginForm';
 export * from 'components/RegistrationForm/RegistrationForm';
-export * from 'components/Layout/SideBar/SideBar.jsx';
+
+export * from 'components/TransitionsList/TransitionsList';
+export * from 'components/TransitionsList/TransitionsListMobile';
+export * from 'components/TransitionsList/LoaderDelBtn.jsx';
+
 export * from 'components/stickyBtn/StickyBtn.jsx';
+
 export * from 'components/Balance/Balance.jsx';
+
 export { default as ModalTeam } from './ModalTeam/ModalTeam';
