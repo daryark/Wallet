cd5f8cd79ead604806474d7868331baca42f8578
Dasha Yarkovska <yarkovska11@gmail.com>
2023-03-12 10:54:59 +0200

    add login/register styles normal

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,2 +1,4 @@
 export { ReactComponent as WalletLogo } from './wallet-logo.svg';
 export { ReactComponent as Exit } from './exit.svg';
+export { ReactComponent as LoginPageSvg } from './loginpage.svg';
+export { ReactComponent as RegisterPageSvg } from './registerpage.svg';
--- src/components/LoginForm/LoginForm.styled.js
+++ src/components/LoginForm/LoginForm.styled.js
@@ -1,15 +1,29 @@
+// import styled from 'styled-components';
+
+// export const LoginBox = styled.div`
+//   padding: 107px 0px 107px;
+
+//   @media screen and (min-width: 768px) {
+//     padding: 40px 0px 62px;
+//     margin: 0 auto;
+//     width: 532px;
+//     height: 468px;
+
+//     background-color: white;
+//     border-radius: 20px;
+//   }
+// `;
+
 import styled from 'styled-components';
 
 export const LoginBox = styled.div`
-  padding: 107px 0px 107px;
-
+  background-color: white;
   @media screen and (min-width: 768px) {
-    padding: 40px 0px 62px;
+    padding: 40px 58px 62px 65px;
     margin: 0 auto;
     width: 532px;
     height: 468px;
 
-    background-color: white;
     border-radius: 20px;
   }
 `;
