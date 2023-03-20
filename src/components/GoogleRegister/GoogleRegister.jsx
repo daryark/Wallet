80c10fe41043817072ef9e5b75cdb10ea87cebe8
Dasha Yarkovska <yarkovska11@gmail.com>
2023-03-21 00:16:28 +0200

    google register login works

--- src/components/GoogleRegister/GoogleRegister.jsx
+++ src/components/GoogleRegister/GoogleRegister.jsx
@@ -1,25 +1,58 @@
 /* eslint-disable no-undef */
-import { useEffect } from 'react';
-// import { FcGoogle } from 'react-icons/fc';
+import { useEffect, useState } from 'react';
+import jwt_decode from 'jwt-decode';
+import { useDispatch } from 'react-redux';
+import { loginRequest, registerRequest } from 'redux/auth/auth-operations';
 import { StyledGoogleRegister } from './GoogleRegister.styled';
 
 export function GoogleRegister() {
+  const [userData, setUserData] = useState(null);
+  const dispatch = useDispatch();
+
   function handleCallbackResponse(response) {
-    console.log('encoded JWT ID token' + response.credential);
+    const userObject = jwt_decode(response.credential);
+    // console.log(userObject);
+
+    setUserData(userObject);
   }
 
   useEffect(() => {
+    /*global google*/
     google.accounts.id.initialize({
       client_id:
         '161461164988-67rsmrpvfbu1oiafg1lro119oa1evv5k.apps.googleusercontent.com',
       callback: handleCallbackResponse,
     });
 
-    google.accounts.id.renderButton(
-      document.getElementById('signInDiv')
-      // {theme: "outline", size: "large" }
-    );
+    // '665888736356-aq6fvfmau6mupt4nfbms5tfch0u2698i.apps.googleusercontent.com',
+
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large',
+      color: 'red',
+    });
   }, []);
+
+  useEffect(() => {
+    const password = userData?.email.split('').reverse().join('');
+
+    //change the pathname to : "/register"
+    if (window.location.pathname === '/Wallet/register') {
+      dispatch(
+        registerRequest({
+          email: userData?.email,
+          password,
+          username: userData?.name,
+        })
+      );
+    }
+    dispatch(
+      loginRequest({
+        email: userData?.email,
+        password,
+      })
+    );
+  }, [dispatch, userData]);
 
   return (
     <StyledGoogleRegister>
