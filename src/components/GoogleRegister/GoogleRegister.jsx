735f48f16800fe1680a85d359e0cee19bf6cc084
Dasha Yarkovska <yarkovska11@gmail.com>
2023-03-20 21:38:45 +0200

    commit

--- src/components/GoogleRegister/GoogleRegister.jsx
+++ src/components/GoogleRegister/GoogleRegister.jsx
@@ -1,19 +1,29 @@
-import { FcGoogle } from 'react-icons/fc';
+/* eslint-disable no-undef */
+import { useEffect } from 'react';
+// import { FcGoogle } from 'react-icons/fc';
 import { StyledGoogleRegister } from './GoogleRegister.styled';
 
 export function GoogleRegister() {
+  function handleCallbackResponse(response) {
+    console.log('encoded JWT ID token' + response.credential);
+  }
+
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        '161461164988-67rsmrpvfbu1oiafg1lro119oa1evv5k.apps.googleusercontent.com',
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById('signInDiv')
+      // {theme: "outline", size: "large" }
+    );
+  }, []);
+
   return (
     <StyledGoogleRegister>
-      <p className="google__text">Sign in with</p>
-      <a
-        className="google__btn"
-        href="https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&client_id=665888736356-aq6fvfmau6mupt4nfbms5tfch0u2698i.apps.googleusercontent.com&prompt=consent&redirect_uri=https%3A%2F%2Fkapusta-backend.goit.global%2Fauth%2Fgoogle-redirect&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FcGoogle width="24" height="24" />
-        google
-      </a>
+      <div id="signInDiv"></div>
     </StyledGoogleRegister>
   );
 }
